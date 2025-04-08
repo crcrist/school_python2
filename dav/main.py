d6c02697d9da2d255a0ba11f518a0ef2f2308743
Connor Crist <connorcrist27@gmaill.com>
2025-04-08 07:48:54 -0500

    found some dupes in data, working to agg on artists + song

--- dav/main.py
+++ dav/main.py
@@ -30,6 +30,34 @@ try:
     null_counts = df.isnull().sum()
     print(null_counts)
 
+    print("""
+          because the level of detail is at the song level, and streams are aggregated, it would not make sense to fill null values with median.
+          ie; if a song was at bottom of list, and we filled with median of spotify streams for whole dataset, would inflate the score of that song.
+
+          now check for duplicate rows
+            """)
+    duplicate_rows = df.duplicated()
+    has_duplicates = duplicate_rows.any()
+    if has_duplicates:
+        print("number of duplicated rows:", duplicate_rows.sum())
+        print("duplicate rows")
+        print(df[duplicate_rows])
+
+    print(f"dataset has duplicate rows {duplicate_rows}")
+    
+    duplicate_songs = df.duplicated(subset=['Track', 'Artist'], keep='first')
+    has_duplicate_songs = duplicate_songs.any()
+    print(f"dataset has duplicate songs {duplicate_songs}")
+    if has_duplicate_songs:
+        print("number of duplicate songs:", duplicate_songs.sum())
+        print("duplicate rows:")
+        print(df[duplicate_songs])
+
+    print("""
+            going to drop the duplicates that are matching over all columns, for the other artist + song combinations i am going to aggregate the metrics
+          """)
+    df = df.drop_duplicates()
+
 except UnicodeDecodeError as e:
     print(f"Error reading csv file: {e}")
     
